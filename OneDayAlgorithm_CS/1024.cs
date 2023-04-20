eeda476a2195ac02e69961315b1ccd030b2b9d11
leejihoo <ajtwlsdl33@gmail.com>
2023-04-20 17:14:38 +0900

    1024 수열의 합
    https://chestnut-vein-aab.notion.site/f86c5469f54142eeabeb009c0f3b70f8

---
+++ OneDayAlgorithm_CS/1024.cs
@@ -0,0 +1,55 @@
+﻿int[] inputs = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+long start = 0;
+long count = 0;
+bool check = false;
+for (long i = inputs[1]; i <= 100; i++)
+{
+    // 시간 초과가 발생하는 코드
+    // for (long j = 0; j < (double)inputs[0]/i; j++)
+    // {
+    //     if (j * i + i * (i - 1) / 2 == inputs[0])
+    //     {
+    //         start = j;
+    //         count = i;
+    //         check = true;
+    //         break;
+    //     }
+    // }
+
+    // https://sexycoder.tistory.com/97 참고
+    if ((inputs[0] - i * (i - 1) / 2) % i == 0 && (inputs[0] - i * (i - 1) / 2) / i >= 0)
+    {
+        start = (inputs[0] - i * (i - 1) / 2) / i;
+        count = i;
+        break;  
+    }
+    
+    // if (check)
+    // {
+    //     break;
+    // }
+
+    if (i == 100)
+    {
+        start = -1;
+    }
+}
+
+if (start == -1)
+{
+    Console.Write(-1);
+}
+else
+{
+    for (int i = 0; i < count; i++)
+    {
+        if (i != count - 1)
+        {
+            Console.Write(start+i+" ");
+        }
+        else
+        {
+            Console.Write(start+i);
+        }
+    }
+}
