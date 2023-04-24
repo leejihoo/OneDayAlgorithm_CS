604cc6f16f39b2604714cd48baa8ed326ad93ed8
leejihoo <ajtwlsdl33@gmail.com>
2023-04-24 15:56:09 +0900

    1138 한 줄 서기
    https://chestnut-vein-aab.notion.site/b891b9f920704e948c4ef501037e8511

---
+++ OneDayAlgorithm_CS/1138.cs
@@ -0,0 +1,39 @@
+﻿int N = int.Parse(Console.ReadLine());
+int[] inputs = Array.ConvertAll(Console.ReadLine().Split(),int.Parse);
+int[] outputs = new int[N];
+
+for (int i = 1; i <= N; i++)
+{
+    int count = 0;
+    for (int j = 0; j < N; j++)
+    {
+        if (count != inputs[i - 1])
+        {
+            if (outputs[j] == 0)
+            {
+                count++;
+            }
+        }
+        else
+        {
+            if (outputs[j] != 0)
+            {
+                continue;
+            }
+            outputs[j] = i;
+            break;
+        }
+    }    
+}
+
+for (int i = 0; i < N; i++)
+{
+    if (i == N - 1)
+    {
+        Console.Write(outputs[i]);
+    }
+    else
+    {
+        Console.Write(outputs[i]+" ");
+    }
+}
