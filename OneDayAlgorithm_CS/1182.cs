5f5d637649759d102c63fd4181183a811c3fa8a6
leejihoo <ajtwlsdl33@gmail.com>
2023-04-25 17:05:35 +0900

    1182 부분수열의 합
    https://chestnut-vein-aab.notion.site/adb168d2d9ec4ec69d5fc0c30d5b71a6
    재귀로 풀면 쉽게 풀린다. 재귀를 생각못해서 헤매다가 검색 후 다른 사람의 코드를 참고하여 풀었다.

---
+++ OneDayAlgorithm_CS/1182.cs
@@ -0,0 +1,19 @@
+﻿int[] inputs = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int[] sequence = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int answer = 0;
+
+void solve(int cur, int sum)
+{
+    if (cur == inputs[0])
+    {
+        if (sum == inputs[1]) answer++;
+        return;
+    }
+    solve(cur+1,sum);
+    solve(cur+1,sum+sequence[cur]);
+}
+
+solve(0,0);
+if (inputs[1] == 0) answer--;
+
+Console.WriteLine(answer);
