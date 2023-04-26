9461653f7c677b6da9e6977f16a93bd36850e09a
leejihoo <ajtwlsdl33@gmail.com>
2023-04-26 16:15:18 +0900

    1183 약속
    틀린 코드인데 반례를 찾지 못해서 일단 저장해둔다.

---
+++ OneDayAlgorithm_CS/1183.cs
@@ -0,0 +1,42 @@
+﻿
+int N = int.Parse(Console.ReadLine());
+int min = 1000000000;
+int[] T = new int[N];
+List<int> select = new List<int>();
+for (int i = 0; i < N; i++)
+{
+    int[] inputs = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+    T[i] = inputs[1] - inputs[0];
+}
+
+foreach (var t in T)
+{
+    int sum = 0;
+    foreach (var t2 in T)
+    {
+        sum += Math.Abs(t - t2);
+    }
+
+    if (min == sum)
+    {
+        select.Add(t);
+    }
+    else if (min > sum)
+    {
+        min = sum;
+        select.Clear();
+        select.Add(t);
+    }
+}
+
+if (select.Count > 1)
+{
+    int max = select.Max();
+    int mini = select.Min();
+    
+    Console.Write(max-mini+1);
+}
+else
+{
+    Console.Write(1);
+}
