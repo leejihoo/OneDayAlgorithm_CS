c90a22a6e33fd08c4a655e75df3eaa45819c7975
leejihoo <ajtwlsdl33@gmail.com>
2023-04-28 23:24:13 +0900

    1157 단어공부
    아스키코드 이용

---
+++ OneDayAlgorithm_CS/1157.cs
@@ -0,0 +1,33 @@
+﻿string input = Console.ReadLine();
+input = input.ToUpper();
+int[] alphabet = new int[26];
+int max = 0;
+bool isTwo = false;
+char answer = ' ';
+for (int i = 0; i<input.Length; i++)
+{
+    alphabet[(input[i]-65)] += 1;
+}
+
+for (int i = 0; i < alphabet.Length; i++)
+{
+    if (alphabet[i] > max)
+    {
+        max = alphabet[i];
+        answer = (char)(i + 65);
+        isTwo = false;
+    }
+    else if (alphabet[i] == max)
+    {
+        isTwo = true;
+    }
+}
+
+if (isTwo)
+{
+    Console.Write("?");    
+}
+else
+{
+    Console.Write(answer);
+}
