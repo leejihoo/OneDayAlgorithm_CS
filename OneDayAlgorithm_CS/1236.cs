0267d473a4c3019af2a2a13cf470eac619d24357
leejihoo <ajtwlsdl33@gmail.com>
2023-04-29 23:56:02 +0900

    1236 성 지키기

---
+++ OneDayAlgorithm_CS/1236.cs
@@ -0,0 +1,47 @@
+﻿int[] input = Array.ConvertAll(Console.ReadLine().Split(),int.Parse);
+string[] castle = new string[input[0]];
+int first = 0;
+int second = 0;
+
+for (int i = 0; i < input[0]; i++)
+{
+    castle[i] = Console.ReadLine();
+}
+
+for (int i = 0; i < input[0]; i++)
+{
+    bool check = false;
+    for (int j = 0; j < input[1]; j++)
+    {
+        if (castle[i][j] == 'X')
+        {
+            check = true;
+            break;
+        }
+    }
+
+    if (!check)
+    {
+        first++;
+    }
+}
+
+for (int i = 0; i < input[1]; i++)
+{
+    bool check = false;
+    for (int j = 0; j < input[0]; j++)
+    {
+        if (castle[j][i] == 'X')
+        {
+            check = true;
+            break;
+        }
+    }
+
+    if (!check)
+    {
+        second++;
+    }
+}
+
+Console.Write((first > second)?first:second);    
