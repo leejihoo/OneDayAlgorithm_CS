cdb060138e4b8174701a0c9268eda58377d9eee1
leejihoo <ajtwlsdl33@gmail.com>
2023-05-08 15:16:56 +0900

    16173 점프왕 쩰리
    https://chestnut-vein-aab.notion.site/ffc91aafd16f412cbb29204587e8e5a3

---
+++ OneDayAlgorithm_CS/16173.cs
@@ -0,0 +1,47 @@
+﻿int N = int.Parse(Console.ReadLine());
+int[][] graph = new int[N][];
+bool check = false;
+bool[,] visit = new bool[N, N];
+for (int i = 0; i < N; i++)
+{
+    graph[i] = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+}
+
+Dfs(0,0);
+if (!check)
+{
+    Console.Write("Hing");
+}
+else
+{
+    Console.Write("HaruHaru");
+}
+
+void Dfs(int row, int col)
+{
+    if (check)
+    {
+        return;
+    }
+    
+    if (row == N-1 && col == N-1)
+    {
+        check = true;
+        return;
+    }
+
+    int temp = graph[row][col];
+    //아래로 이동
+    if (row + temp < N && !visit[row+temp,col])
+    {
+        visit[row + temp, col] = true;
+        Dfs(row + temp,col);
+    }
+
+    //오른쪽으로 이동
+    if (col + temp< N && !visit[row,col+temp])
+    {
+        visit[row, col + temp] = true;
+        Dfs(row,col + temp);
+    }
+}
