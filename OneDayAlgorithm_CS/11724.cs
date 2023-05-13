54e7178aa3d0d9cd96c5a9e870e145527198996b
leejihoo <ajtwlsdl33@gmail.com>
2023-05-13 16:16:15 +0900

    11724 연결 요소의 개수
    https://chestnut-vein-aab.notion.site/f5336611b4d74c7b979790d8c1f7ccbd

---
+++ OneDayAlgorithm_CS/11724.cs
@@ -0,0 +1,35 @@
+﻿int[] input = Array.ConvertAll(Console.ReadLine().Split(),int.Parse);
+int[,] graph = new int[input[0], input[0]];
+bool[] check = new bool[input[0]];
+int answer = 0;
+for (int i = 0; i < input[1]; i++)
+{
+    int[] edge = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+    graph[edge[0] - 1, edge[1] - 1] = 1;
+    graph[edge[1] - 1, edge[0] - 1] = 1;
+}
+
+for (int i = 0; i < input[0]; i++)
+{
+    if (check[i] == false)
+    {
+        answer++;
+        check[i] = true;
+        Dfs(i);
+    }
+}
+
+Console.Write(answer);
+
+void Dfs(int row)
+{
+    for (int i = 0; i < input[0]; i++)
+    {
+        if (check[i] == false && graph[row, i] == 1)
+        {
+            check[i] = true;
+            Dfs(i);
+        }
+    }
+}
+
