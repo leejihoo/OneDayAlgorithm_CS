c3dd85f2c21af9393577c4b6a088e7afd133aadd
leejihoo <ajtwlsdl33@gmail.com>
2023-05-14 17:20:58 +0900

    2738 행렬 덧셈
    잔디 방어용

---
+++ OneDayAlgorithm_CS/2738.cs
@@ -0,0 +1,25 @@
+﻿int[] length = Array.ConvertAll(Console.ReadLine().Split(),int.Parse);
+int[][] first = new int[length[0]][];
+int[][] second = new int[length[0]][];
+
+for (int i = 0; i < length[0]; i++)
+{
+    first[i] = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+}
+for (int i = 0; i < length[0]; i++)
+{
+    second[i] = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+}
+
+for (int i = 0; i < length[0]; i++)
+{
+    for (int j = 0; j < length[1]; j++)
+    {
+        Console.Write(first[i][j]+second[i][j]);
+        if (j != length[1] - 1)
+        {
+            Console.Write(" ");
+        }
+    }
+    Console.WriteLine();
+}
