842a63ae6f71c79557e51dc56a220cc0eda24185
leejihoo <ajtwlsdl33@gmail.com>
2023-05-24 16:23:36 +0900

    10815 숫자 카드
    Hint: hashset
    
    https://chestnut-vein-aab.notion.site/4b973d86302942f7ae89afb546ab016a

---
+++ OneDayAlgorithm_CS/10815.cs
@@ -0,0 +1,22 @@
+﻿int N = int.Parse(Console.ReadLine());
+int[] cards = Array.ConvertAll(Console.ReadLine().Split(),int.Parse);
+int M = int.Parse(Console.ReadLine());
+int[] question = Array.ConvertAll(Console.ReadLine().Split(),int.Parse);
+HashSet<int> hashSetCards = cards.ToHashSet();
+
+for (int i = 0; i < question.Length; i++)
+{
+    if (hashSetCards.Contains(question[i]))
+    { 
+        question[i] = 1;
+    }
+    else
+    {
+        question[i] = 0;
+    }
+}
+
+Console.Write(string.Join(" ", question));
+
+    
+    
