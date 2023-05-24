32bd453ee2678eb8de674673ca630e4331eb5ff9
leejihoo <ajtwlsdl33@gmail.com>
2023-05-24 15:12:11 +0900

    1068 트리
    https://chestnut-vein-aab.notion.site/4b973d86302942f7ae89afb546ab016a

---
+++ OneDayAlgorithm_CS/1068.cs
@@ -0,0 +1,48 @@
+﻿int N = int.Parse(Console.ReadLine());
+int[] parent = Array.ConvertAll(Console.ReadLine().Split(),int.Parse);
+int deletedNode = int.Parse(Console.ReadLine());
+int count = 0;
+int start = 0;
+List<List<int>> tree = new List<List<int>>();
+
+for (int i = 0; i < N; i++)
+{
+    tree.Add(new List<int>());
+}
+
+for (int i = 0; i < N; i++)
+{
+    if (parent[i] != -1)
+    {
+        tree[parent[i]].Add(i);    
+    }
+    else
+    {
+        start = i;
+    }
+}
+
+Dfs(start);
+Console.Write(count);
+
+void Dfs(int node)
+{
+    if (node != deletedNode)
+    {
+        if (tree[node].Count > 0)
+        {
+            for (int i = 0; i < tree[node].Count; i++)
+            {
+                if (tree[node].Count == 1 && tree[node][i] == deletedNode)
+                {
+                    count++;
+                }
+                Dfs(tree[node][i]); 
+            }    
+        }
+        else
+        {
+            count++;
+        }
+    }
+}
