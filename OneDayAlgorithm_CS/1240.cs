e0868c0d5ba2b028e2bb2fa03029a2f2d067375b
leejihoo <ajtwlsdl33@gmail.com>
2023-05-25 15:45:20 +0900

    1240 노드사이의 거리
    https://chestnut-vein-aab.notion.site/6d08d4b68b29485c92e9aaf396177d05

---
+++ OneDayAlgorithm_CS/1240.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.Design;
+
+int [] mn = Array.ConvertAll(Console.ReadLine().Split(),int.Parse);
+int[,] graph = new int[mn[0],mn[0]];
+bool[] check = new bool[mn[0]];
+int output = 0;
+// edge의 갯수는 node-1개
+for (int i = 0; i < mn[0]-1; i++)
+{
+    int[] inputs = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+    graph[inputs[0] - 1, inputs[1] - 1] = inputs[2];
+    graph[inputs[1] - 1, inputs[0] - 1] = inputs[2];
+}
+
+for (int i = 0; i < mn[1]; i++)
+{
+    int[] inputs = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+    Dfs(inputs[0],inputs[1],0);
+    
+    for (int j = 0; j < check.Length; j++)
+    {
+        check[j] = false;
+    }
+    Console.WriteLine(output);
+}
+
+void Dfs(int start, int end, int answer)
+{
+    if (!check[start-1])
+    {
+        check[start-1] = true;
+        if (graph[start - 1, end - 1] != 0)
+        {
+            output = answer + graph[start - 1, end - 1];
+            return;
+        }
+        
+        for (int i = 0; i < mn[0]; i++)
+        {
+            if (graph[start-1, i] != 0)
+            {
+                Dfs(i+1, end, answer + graph[start-1, i]);
+            }
+        }
+    }
+}
