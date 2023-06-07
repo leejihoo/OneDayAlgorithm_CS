780d9205e24278525c4762ea330f0783c02aa49a
leejihoo <ajtwlsdl33@gmail.com>
2023-06-07 15:58:50 +0900

    1987 알파벳
    https://chestnut-vein-aab.notion.site/24b2111fa1f84ecaaf6ed67926a2ba23?pvs=4

---
+++ OneDayAlgorithm_CS/1987.cs
@@ -0,0 +1,52 @@
+﻿int[] rc = Array.ConvertAll(Console.ReadLine().Split(),int.Parse);
+string[] graph = new string[rc[0]];
+bool[,] check = new bool[rc[0], rc[1]];
+int max = 0;
+List<char> alphabet = new List<char>();
+
+for (int i = 0; i < rc[0]; i++)
+{
+    graph[i] = Console.ReadLine();
+}
+
+Dfs(0,0,0);
+Console.Write(max);
+
+void Dfs(int row, int col, int count)
+{
+    if (check[row, col] || alphabet.Contains(graph[row][col]))
+    {
+        if (count > max)
+        {
+            max = count;
+        }
+        return;
+    }
+    alphabet.Add(graph[row][col]);
+    count++;
+    check[row, col] = true;
+    
+    //상
+    if (row - 1 >= 0)
+    {
+        Dfs(row-1,col,count);
+    }
+    //하
+    if (row + 1 < rc[0])
+    {
+        Dfs(row+1,col,count);
+    }
+    //좌
+    if (col - 1 >= 0)
+    {
+        Dfs(row,col-1,count);
+    }
+    //우
+    if (col + 1 < rc[1])
+    {
+        Dfs(row,col+1,count);
+    }
+    
+    alphabet.Remove(graph[row][col]);
+    check[row, col] = false;
+}
