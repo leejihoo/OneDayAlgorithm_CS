9f07b546716dd9035ac243c7967b8d51402fae46
leejihoo <ajtwlsdl33@gmail.com>
2023-06-09 18:32:08 +0900

    17471 게리맨더링
    https://chestnut-vein-aab.notion.site/23f1f73789fb49cfbf7a74377a9d2f67?pvs=4
    부분수열과 dfs를 사용해서 풀었다.

--- OneDayAlgorithm_CS/17471.cs
+++ OneDayAlgorithm_CS/17471.cs
@@ -1,106 +1,109 @@
-﻿// int N = int.Parse(Console.ReadLine());
-// int[] population = Array.ConvertAll(Console.ReadLine().Split(),int.Parse);
-// List<int>[] graph = new List<int>[N+1];
-// bool[] check = new bool[N + 1];
-// int redPopulation = 0;
-// int bluePopulation = 0;
-// int min = 1000;
-// check[0] = true;
-//
-// for (int i = 0; i < N + 1; i++)
-// {
-//     graph[i] = new List<int>();
-// }
-//
-// for (int i = 1; i <= N; i++)
-// {
-//     int[] input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-//     for (int j = 1; j <= input[0]; j++)
-//     {
-//         graph[i].Add(input[j]);
-//     }
-// }
-//
-// for (int i = 1; i <= N; i++)
-// {
-//     DfsRed(i);
-// }
-//
-// if (min != 1000)
-// {
-//     Console.WriteLine(min);
-// }
-// else
-// {
-//     Console.WriteLine(-1);
-// }
-//
-// void DfsRed(int start)
-// {
-//     bool[] temp = new bool[N + 1];
-//     
-//     if (check[start])
-//     {
-//         return;
-//     }
-//
-//     redPopulation += population[start-1];
-//     check[start] = true;
-//     for (int i = 0; i < N+1; i++)
-//     {
-//         temp[i] = check[i];
-//     }
-//     
-//     for (int i = 1; i <= N; i++)
-//     {
-//         if (!check[i])
-//         {
-//             DfsBlue(i);
-//             break;
-//         }
-//     }
-//
-//     bluePopulation = 0;
-//     for (int i = 0; i < N+1; i++)
-//     {
-//         check[i] = temp[i];
-//     }
-//     
-//     for (int i = 0; i < graph[start].Count; i++)
-//     {
-//         DfsRed(graph[start][i]);
-//     }
-//     
-//     redPopulation -= population[start-1];
-//     check[start] = false;
-// }
-//
-// void DfsBlue(int start)
-// {
-//     
-//     
-//     if (check[start])
-//     {
-//         return;
-//     }
-//
-//     bluePopulation += population[start-1];
-//     check[start] = true;
-//     
-//     if (!check.Contains(false))
-//     {
-//         int diff = Math.Abs(bluePopulation - redPopulation);
-//         if (diff < min)
-//         {
-//             min = diff;
-//         }
-//     }
-//     else
-//     {
-//         for (int i = 0; i < graph[start].Count; i++)
-//         {
-//             DfsBlue(graph[start][i]);
-//         }
-//     }
-//     
-// }
+﻿int N = int.Parse(Console.ReadLine());
+int[] population = Array.ConvertAll(Console.ReadLine().Split(),int.Parse);
+List<int>[] graph = new List<int>[N+1];
+bool[] check = new bool[N + 1];
+char[] colors = new char[N + 1];
+int redPopulation = 0;
+int bluePopulation = 0;
+int min = 1000;
+int count = 0;
+bool isCounted = false;
+
+check[0] = true;
+
+for (int i = 0; i < N + 1; i++)
+{
+    graph[i] = new List<int>();
+}
+
+for (int i = 1; i <= N; i++)
+{
+    int[] input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+    for (int j = 1; j <= input[0]; j++)
+    {
+        graph[i].Add(input[j]);
+    }
+}
+
+Divide(0,'n',colors);
+
+if (min != 1000)
+{
+    Console.WriteLine(min);
+}
+else
+{
+    Console.WriteLine(-1);
+}
+
+void Divide(int cur, char color, char[] next)
+{
+    char[] temp = new char[N + 1];
+    
+    for (int i = 1; i <= N; i++)
+    {
+        temp[i] = next[i];
+    }
+    temp[cur] = color;
+    
+    if (cur == N)
+    {
+        colors = temp;
+        for (int i = 1; i <= N; i++)
+        {
+            Dfs(i,'r');
+            Dfs(i,'b');
+            isCounted = false;
+        }
+        Array.Fill(check,false);
+        
+        if (count == 2)
+        {
+            int diff = Math.Abs(redPopulation - bluePopulation);
+            if (diff < min)
+            {
+                min = diff;
+            }
+        }
+
+        redPopulation = 0;
+        bluePopulation = 0;
+        count = 0;
+    }
+    else
+    {
+        Divide(cur+1,'r',temp);
+        Divide(cur+1,'b',temp);
+    }
+}
+
+void Dfs(int start, char color)
+{
+    if (check[start] || color != colors[start])
+    {
+        return;
+    }
+
+    if (!isCounted)
+    {
+        isCounted = true;
+        count++;
+    }
+
+    if (color == 'r')
+    {
+        redPopulation += population[start-1];
+    }
+    else
+    {
+        bluePopulation += population[start-1];
+    }
+    
+    check[start] = true;
+
+    for (int i = 0; i < graph[start].Count; i++)
+    {
+        Dfs(graph[start][i],color);
+    }
+}
+
