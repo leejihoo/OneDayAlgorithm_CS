e1ddd2f52477909e3cfc519c83b555cb5c093d3e
leejihoo <ajtwlsdl33@gmail.com>
2023-06-12 16:24:56 +0900

    16953 A->B
    https://chestnut-vein-aab.notion.site/fa3f6e7a81fe452dbd73286042ea2de9?pvs=4

---
+++ OneDayAlgorithm_CS/16953.cs
@@ -0,0 +1,42 @@
+﻿long[] ab = Array.ConvertAll(Console.ReadLine().Split(),long.Parse);
+Queue<long> graph = new Queue<long>();
+int count = 0;
+bool check = false;
+graph.Enqueue(ab[0]);
+while (true)
+{
+    count++;
+    int repeat = graph.Count;
+    for (int i = 0; i < repeat; i++)
+    {
+        long node = graph.Dequeue();
+
+        if (node > ab[1])
+        {
+            continue;
+        }
+        
+        if (node == ab[1])
+        {
+            check = true;
+            break;
+        }
+        
+        graph.Enqueue(node * 10 + 1);
+        graph.Enqueue(node * 2);
+    }
+
+    if (check || repeat == 0)
+    {
+        break;
+    }
+}
+
+if(check)
+{
+    Console.Write(count);
+}
+else
+{
+    Console.Write(-1);
+}
