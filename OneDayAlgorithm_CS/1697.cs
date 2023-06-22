deeb00ab83305d31c7253ede0f626d26230b9b00
leejihoo <ajtwlsdl33@gmail.com>
2023-06-22 14:59:32 +0900

    1697 숨바꼭질
    https://chestnut-vein-aab.notion.site/0465140c0e5a42ff971b831f264200a5?pvs=4

---
+++ OneDayAlgorithm_CS/1697.cs
@@ -0,0 +1,42 @@
+﻿int[] nk = Array.ConvertAll(Console.ReadLine().Split(),int.Parse);
+Queue<int> queue = new Queue<int>();
+bool[] check = new bool[100001];
+int answer = -1;
+queue.Enqueue(nk[0]);
+check[nk[0]] = true;
+while (queue.Count != 0)
+{
+    answer++;
+    int count = queue.Count;
+    
+    for (int i = 0; i < count; i++)
+    {
+        int node = queue.Dequeue();
+        if (node == nk[1])
+        {
+            queue.Clear();
+            break;
+        }
+        
+        if (node -1 >= 0 && node -1 <= 100000 && !check[node - 1])
+        {
+            check[node - 1] = true;
+            queue.Enqueue(node-1);
+        }
+
+        if (node + 1 >= 0 && node + 1 <= 100000 && !check[node + 1])
+        {
+            check[node + 1] = true;
+            queue.Enqueue(node+1);
+        }
+
+        if (node * 2 >= 0 && node * 2 <= 100000 && !check[node * 2])
+        {
+            check[node * 2] = true;
+            queue.Enqueue(node*2);
+        }
+    }
+    
+}
+
+Console.Write(answer);
