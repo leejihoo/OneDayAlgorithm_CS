d0dcae122fc1a7ffdce24c94db5d28e93fc7f471
leejihoo <ajtwlsdl33@gmail.com>
2023-07-03 23:31:08 +0900

    1316 그룹 단어 체커
    https://chestnut-vein-aab.notion.site/15edaf37d1394560ad8dff390dd1bcb7?pvs=4

---
+++ OneDayAlgorithm_CS/1316.cs
@@ -0,0 +1,33 @@
+﻿int N = int.Parse(Console.ReadLine());
+int count = 0;
+
+for (int i = 0; i < N; i++)
+{
+    string word = Console.ReadLine();
+    char prev = '1';
+    List<char> spelling = new List<char>();
+    bool check = true;
+    for (int j = 0; j < word.Length; j++)
+    {
+        if (prev != word[j])
+        {
+            if (spelling.Contains(word[j]))
+            {
+                check = false;
+                break;
+            }
+            
+            spelling.Add(word[j]); 
+            prev = word[j];
+            
+        }
+    }
+
+    if (check)
+    {
+        count++;
+    }
+}
+
+Console.Write(count);
+
