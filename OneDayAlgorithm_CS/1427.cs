e34ba8ed87e99bb53161831ef9c9943b2d423ac0
leejihoo <ajtwlsdl33@gmail.com>
2023-07-04 23:42:40 +0900

    1427 소트인사이드
    https://chestnut-vein-aab.notion.site/4b1cac39cc794d4ea6b9555139010772?pvs=4

---
+++ OneDayAlgorithm_CS/1427.cs
@@ -0,0 +1,14 @@
+﻿string input = Console.ReadLine();
+List<int> list = new List<int>();
+
+for (int i = 0; i < input.Length; i++)
+{
+    list.Add((input[i] - '0'));    
+}
+
+list.Sort();
+list.Reverse();
+foreach (var num in list)
+{
+    Console.Write(num);
+}
