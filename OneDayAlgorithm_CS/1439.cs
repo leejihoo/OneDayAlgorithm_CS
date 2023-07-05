310ab29662ab97bd747e654c05d6428c932e2e7a
leejihoo <ajtwlsdl33@gmail.com>
2023-07-05 23:52:32 +0900

    1439 뒤집기
    https://chestnut-vein-aab.notion.site/695f82f6b07c4039b070fd69359e5cce?pvs=4

---
+++ OneDayAlgorithm_CS/1439.cs
@@ -0,0 +1,53 @@
+﻿string input = Console.ReadLine();
+int zeroSequenceCount = 0;
+int oneSequenceCount = 0;
+char prev = 'n';
+for (int i = 0; i < input.Length; i++)
+{
+    if (prev == 'n')
+    {
+        prev = input[i];
+    }
+
+    if (prev != input[i])
+    {
+        if (prev == '0')
+        {
+            zeroSequenceCount++;
+        }
+        else
+        {
+            oneSequenceCount++;
+        }
+
+        prev = input[i];
+    }
+
+    if (i == input.Length - 1)
+    {
+        if (input[i] == '0')
+        {
+            zeroSequenceCount++;
+        }
+        else
+        {
+            oneSequenceCount++;
+        }
+    }
+}
+
+if (zeroSequenceCount * oneSequenceCount == 0)
+{
+    Console.Write(0);
+}
+else
+{
+    if (zeroSequenceCount > oneSequenceCount)
+    {
+        Console.Write(oneSequenceCount);
+    }
+    else
+    {
+        Console.Write(zeroSequenceCount);
+    }
+}
