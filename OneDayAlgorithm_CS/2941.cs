a9a134c2a7c838135c73b3d576ac526461d63d58
leejihoo <ajtwlsdl33@gmail.com>
2023-07-06 23:31:08 +0900

    2941 크로아티아 알파벳
    https://chestnut-vein-aab.notion.site/d85c4692f8334fe5897793d17011fac5?pvs=4

---
+++ OneDayAlgorithm_CS/2941.cs
@@ -0,0 +1,77 @@
+﻿string input = Console.ReadLine();
+int count = 0;
+for (int i = 0; i < input.Length; i++)
+{
+    if (input[i] == 'c')
+    {
+        if (i + 1 < input.Length)
+        {
+            if (input[i + 1] == '=' || input[i + 1] == '-')
+            {
+                i++;
+            }
+        }
+    }
+    else if (input[i] == 'd')
+    {
+        if (i + 2 < input.Length)
+        {
+            if (input[i + 1] == 'z' && input[i + 2] == '=')
+            {
+                i += 2;
+            }
+        }
+        
+        if (i + 1 < input.Length)
+        {
+            if (input[i + 1] == '-')
+            {
+                i += 1;
+            }
+        }
+    }
+    else if (input[i] == 'l')
+    {
+        if (i + 1 < input.Length)
+        {
+            if (input[i + 1] == 'j')
+            {
+                i++;
+            }
+        }
+    }
+    else if (input[i] == 'n')
+    {
+        if (i + 1 < input.Length)
+        {
+            if (input[i + 1] == 'j')
+            {
+                i++;
+            }
+        }
+    }
+    else if (input[i] == 's')
+    {
+        if (i + 1 < input.Length)
+        {
+            if (input[i + 1] == '=')
+            {
+                i++;
+            }
+        }
+    }
+    else if (input[i] == 'z')
+    {
+        if (i + 1 < input.Length)
+        {
+            if (input[i + 1] == '=')
+            {
+                i++;
+            }
+        }
+    }
+
+    count++;
+}
+
+Console.Write(count);
