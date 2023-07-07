e9aa3ad86791025da79519ecc04fea4cade844d0
leejihoo <ajtwlsdl33@gmail.com>
2023-07-07 23:36:31 +0900

    25206 너의 평점은
    https://chestnut-vein-aab.notion.site/f7d94ba3435a480fb8d0e73e8cae7824?pvs=4

---
+++ OneDayAlgorithm_CS/25206.cs
@@ -0,0 +1,46 @@
+﻿float gradeSum = 0.0f;
+float Cal = 0.0f;
+
+for (int i = 0; i < 20; i++)
+{
+    string[] inputs = Console.ReadLine().Split();
+
+    if (inputs[2] != "P")
+    {
+        float grade = float.Parse(inputs[1]);
+        gradeSum += grade;
+        switch(inputs[2])
+        {
+            case "A+":
+                Cal += 4.5f * grade;
+                break;
+            case "A0":
+                Cal += 4.0f * grade;
+                break;
+            case "B+":
+                Cal += 3.5f * grade;
+                break;
+            case "B0":
+                Cal += 3.0f * grade;
+                break;
+            case "C+":
+                Cal += 2.5f * grade;
+                break;
+            case "C0":
+                Cal += 2.0f * grade;
+                break;
+            case "D+":
+                Cal += 1.5f * grade;
+                break;
+            case "D0":
+                Cal += 1.0f * grade;
+                break;
+            case "F":
+                Cal += 0.0f * grade;
+                break;
+
+        }
+    }
+}
+
+Console.Write(Cal / gradeSum);
