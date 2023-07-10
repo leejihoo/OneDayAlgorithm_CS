800e701091eece605a8a2fc2550d9a9c39e30e8a
leejihoo <ajtwlsdl33@gmail.com>
2023-07-10 23:36:14 +0900

    1120 문자열
    https://chestnut-vein-aab.notion.site/bdaf824bab1b48e98217aa1ed4086a1f?pvs=4

---
+++ OneDayAlgorithm_CS/1120.cs
@@ -0,0 +1,20 @@
+﻿string[] inputs = Console.ReadLine().Split();
+int repeat = inputs[1].Length - inputs[0].Length + 1;
+int answer = 51;
+for (int i = 0; i < repeat; i++)
+{
+    int diff = 0;
+    for (int j = 0; j < inputs[0].Length; j++)
+    {
+        if (inputs[1][i + j] != inputs[0][j])
+        {
+            diff++;
+        }
+    }
+
+    if (answer > diff)
+    {
+        answer = diff;
+    }
+}
+Console.Write(answer);
