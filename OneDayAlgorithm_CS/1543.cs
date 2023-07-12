e86e68976cfd612c67171c58a60673de49428b0e
leejihoo <ajtwlsdl33@gmail.com>
2023-07-12 20:49:34 +0900

    1543 문서 검색
    https://chestnut-vein-aab.notion.site/8a17911d31624fa7a7c2e650c75f402f?pvs=4

---
+++ OneDayAlgorithm_CS/1543.cs
@@ -0,0 +1,29 @@
+﻿string document = Console.ReadLine();
+string word = Console.ReadLine();
+int answer = 0;
+for (int i = 0; i < document.Length; i++)
+{
+    // 탐색할려는 단어보다 남은 문자열이 짧음
+    if (document.Length - i < word.Length)
+    {
+        break;
+    }
+
+    for (int j = 0; j < word.Length; j++)
+    {
+        if (word[j] != document[j + i])
+        {
+            break;
+        }
+
+        if (j == word.Length - 1)
+        {
+            answer++;
+            //반복문에서 +1 해주기 때문에 -1을 해줘야한다.
+            i += word.Length-1;
+        }
+    }
+}
+
+Console.Write(answer);
+
