2b021bbf4f4b07857c8d68a77b93031a53af1bf9
leejihoo <ajtwlsdl33@gmail.com>
2023-07-13 23:30:33 +0900

    10610 30
    https://chestnut-vein-aab.notion.site/6e5affce89854304aa5e587d1b2af62b?pvs=4

---
+++ OneDayAlgorithm_CS/10610.cs
@@ -0,0 +1,20 @@
+﻿string inputs = Console.ReadLine();
+List<int> changedInputsToInt = new List<int>();
+int sum = 0;
+for (int i = 0; i < inputs.Length; i++)
+{
+    int temp = inputs[i] - '0';
+    sum += temp;
+    changedInputsToInt.Add(temp);
+}
+
+if (changedInputsToInt.Contains(0) && sum % 3 == 0)
+{
+    changedInputsToInt.Sort();
+    changedInputsToInt.Reverse();
+    Console.Write(String.Join("",changedInputsToInt));
+}
+else
+{
+    Console.Write(-1);
+}
