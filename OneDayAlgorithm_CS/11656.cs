48ec13869172407d647848c449c653d5b0350bb9
leejihoo <ajtwlsdl33@gmail.com>
2023-07-18 23:46:52 +0900

    11656번 접미사 배열
    https://chestnut-vein-aab.notion.site/b9521b23a3a84a8e9aaef8597abc687a?pvs=4

---
+++ OneDayAlgorithm_CS/11656.cs
@@ -0,0 +1,13 @@
+﻿string input = Console.ReadLine();
+List<string> list = new List<string>();
+int count = input.Length;
+for (int i = 0; i < count; i++)
+{
+    list.Add(input);
+    input = input.Substring(1);
+}
+list.Sort();
+foreach (var temp in list)
+{
+    Console.WriteLine(temp);
+}
