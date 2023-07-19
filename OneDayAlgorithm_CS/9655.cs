9fb7394e5ad014603ff13a076f2991e9b0889c20
leejihoo <ajtwlsdl33@gmail.com>
2023-07-19 13:17:01 +0900

    9655 돌 게임
    https://chestnut-vein-aab.notion.site/b49e0861963a466e8a0fee28e416d872?pvs=4

---
+++ OneDayAlgorithm_CS/9655.cs
@@ -0,0 +1,23 @@
+﻿int N = int.Parse(Console.ReadLine());
+int[] dp = new int[N+1];
+dp[0] = 0;
+
+for (int i = 1; i <= N; i++)
+{
+    int temp1 = 1001;
+    int temp2 = 1001;
+    
+    if (i - 1 >= 0)
+    {
+        temp1 = dp[i - 1] + 1;
+    }
+
+    if (i - 3 >= 0)
+    {
+        temp2 = dp[i - 3] + 1;
+    }
+
+    dp[i] = temp1 < temp2? temp1 : temp2;
+}
+
+Console.Write(dp[N]%2 == 0?"CY":"SK");
