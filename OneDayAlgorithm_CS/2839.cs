b930ee212771a8c54666a3604289b613089ddb10
leejihoo <ajtwlsdl33@gmail.com>
2023-07-20 23:38:22 +0900

    2839 설탕 배달
    https://chestnut-vein-aab.notion.site/bc14db0548c14efbbd5e32105efa3147?pvs=4

---
+++ OneDayAlgorithm_CS/2839.cs
@@ -0,0 +1,48 @@
+﻿int N = int.Parse(Console.ReadLine());
+int[] dp = new int[5001];
+dp[0] = 0;
+dp[1] = -1;
+dp[2] = -1;
+dp[3] = 1;
+dp[4] = -1;
+dp[5] = 1;
+for (int i = 6; i < N+1; i++)
+{
+    int temp3 = -1;
+    int temp5 = -1;
+    
+    if (i - 3 >= 0)
+    {
+        if (dp[i - 3] != -1)
+        {
+            temp3 = dp[i - 3] + dp[3];
+        }    
+    }
+
+    if (i - 5 >= 0)
+    {
+        if (dp[i - 5] != -1)
+        {
+            temp5 = dp[i - 5] + dp[5];
+        }
+    }
+
+    if (temp3 != -1 && temp5 != -1)
+    {
+        dp[i] = temp3 < temp5? temp3:temp5;
+    }
+    else if (temp3 == -1 && temp5 != -1)
+    {
+        dp[i] = temp5;
+    }
+    else if (temp3 != -1 && temp5 == -1)
+    {
+        dp[i] = temp3;
+    }
+    else
+    {
+        dp[i] = -1;
+    }
+}
+
+Console.Write(dp[N]);
