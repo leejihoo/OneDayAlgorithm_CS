07b498457f702a314fe2ddac07ac6db8748c8b06
leejihoo <ajtwlsdl33@gmail.com>
2023-07-22 16:32:32 +0900

    1904 01타일
    https://chestnut-vein-aab.notion.site/fd9e522021aa44a58e6d90e752405758?pvs=4

---
+++ OneDayAlgorithm_CS/1904.cs
@@ -0,0 +1,10 @@
+﻿int N = int.Parse(Console.ReadLine());
+int[] dp = new int[1000001];
+dp[0] = 0;
+dp[1] = 1;
+dp[2] = 2;
+for (int i = 3; i <= N; i++)
+{
+    dp[i] = (dp[i - 1] + dp[i - 2]) %15746;
+}
+Console.Write(dp[N]);
