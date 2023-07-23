d1de0088681e48a3e1eb6a526a9c497eac6dde60
leejihoo <ajtwlsdl33@gmail.com>
2023-07-23 14:47:46 +0900

    2193 이친수
    https://chestnut-vein-aab.notion.site/1bbb57798e6944b4b95ddee1b9e004ed?pvs=4

---
+++ OneDayAlgorithm_CS/2193.cs
@@ -0,0 +1,10 @@
+﻿int N = int.Parse(Console.ReadLine());
+long[] dp = new long[N+2];
+dp[0] = 1;
+dp[1] = 1;
+dp[2] = 1;
+for (int i = 3; i <= N; i++)
+{
+    dp[i] = dp[i - 1] + dp[i - 2];
+}
+Console.Write(dp[N]);
