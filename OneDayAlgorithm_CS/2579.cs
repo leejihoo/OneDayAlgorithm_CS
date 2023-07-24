4bd12dfc657532106530d9270fd34f9b34d16cb8
leejihoo <ajtwlsdl33@gmail.com>
2023-07-24 23:44:04 +0900

    2579 계단 오르기
    https://chestnut-vein-aab.notion.site/b65e941a5527451a933691ed77fbe617?pvs=4

---
+++ OneDayAlgorithm_CS/2579.cs
@@ -0,0 +1,19 @@
+﻿int N = int.Parse(Console.ReadLine());
+long[] dp = new long[N + 2];
+int[] score = new int[N + 2];
+score[0] = 0;
+for (int i = 1; i < N+1; i++)
+{
+    score[i] = int.Parse(Console.ReadLine());
+}
+
+dp[0] = 0;
+dp[1] = score[1];
+dp[2] = score[1] + score[2];
+
+for (int i = 3; i < N + 1; i++)
+{
+    dp[i] = Math.Max(score[i - 1] + dp[i - 3], dp[i - 2]) + score[i];
+}
+
+Console.Write(dp[N]);
