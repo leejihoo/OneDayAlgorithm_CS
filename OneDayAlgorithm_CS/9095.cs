9981d7dee8b45ff03e7bd086a0212c1d199da608
leejihoo <ajtwlsdl33@gmail.com>
2023-07-25 23:12:21 +0900

    9095번 1,2,3 더하기
    https://chestnut-vein-aab.notion.site/d7df822b936c40e8b4d700634ae29aaf?pvs=4

---
+++ OneDayAlgorithm_CS/9095.cs
@@ -0,0 +1,15 @@
+﻿int T = int.Parse(Console.ReadLine());
+int[] dp = new int[12];
+dp[0] = 1;
+dp[1] = 1;
+dp[2] = 2;
+for (int i = 3; i < 12; i++)
+{
+    // 맨 앞이 1일 때, 2일 때, 3일 때 
+    dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+}
+
+for (int i = 0; i < T; i++)
+{
+    Console.WriteLine(dp[int.Parse(Console.ReadLine())]);
+}
