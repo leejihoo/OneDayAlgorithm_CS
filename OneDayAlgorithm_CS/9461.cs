73813d6153c5c0d21fa9e1716de905aa825495dc
leejihoo <ajtwlsdl33@gmail.com>
2023-07-26 23:57:18 +0900

    9461 파도반 수열
    https://chestnut-vein-aab.notion.site/3c4ab2c38a654da5a92dfc2f7844bbec?pvs=4

---
+++ OneDayAlgorithm_CS/9461.cs
@@ -0,0 +1,20 @@
+﻿int T = int.Parse(Console.ReadLine());
+long[] dp = new long[101];
+dp[0] = 0;
+dp[1] = 1;
+dp[2] = 1;
+dp[3] = 1;
+dp[4] = 2;
+dp[5] = 2;
+dp[6] = 3;
+dp[7] = 4;
+dp[8] = 5;
+for (int i = 9; i < 101; i++)
+{
+    dp[i] = dp[i - 2] + dp[i - 3];
+}
+
+for (int i = 0; i < T; i++)
+{
+   Console.WriteLine(dp[int.Parse(Console.ReadLine())]); 
+}
