fbac8b25dd3d0780a3324ac02b93a35a5c7f4c4c
leejihoo <ajtwlsdl33@gmail.com>
2023-07-28 23:25:14 +0900

    11727번 2*n 타일링2
    https://chestnut-vein-aab.notion.site/2-n-2-50de2464eeda4d078dc18c40e9ec78fa?pvs=4

---
+++ OneDayAlgorithm_CS/11727.cs
@@ -0,0 +1,11 @@
+﻿int n = int.Parse(Console.ReadLine());
+long[]dp= new long[n+2];
+dp[0] = 0;
+dp[1] = 1;
+dp[2] = 3;
+for (int i = 3; i < n + 1; i++)
+{
+    dp[i] = (dp[i - 1] + dp[i - 2] * 2) % 10007;
+}
+
+Console.Write(dp[n]);
