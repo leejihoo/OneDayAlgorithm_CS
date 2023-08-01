28382612b6ba934299843c2ef1fed15138f485b8
leejihoo <ajtwlsdl33@gmail.com>
2023-08-02 00:31:55 +0900

    1912 연속합
    https://chestnut-vein-aab.notion.site/fef23f0caa9c4c74831c259e344b924f?pvs=4

---
+++ OneDayAlgorithm_CS/1912.cs
@@ -0,0 +1,36 @@
+﻿int N = int.Parse(Console.ReadLine());
+int[] inputs = Array.ConvertAll(Console.ReadLine().Split(),int.Parse);
+long[] dp = new long[N];
+long max = -1001;
+dp[0] = inputs[0];
+max = dp[0];
+
+for (int i = 1; i < N; i++)
+{
+    long temp = dp[i - 1] + inputs[i];
+    if (temp > dp[i - 1])
+    {
+        if (max < temp)
+        {
+            max = temp;
+        }
+        
+    }
+    
+
+    if (temp < inputs[i])
+    {
+        dp[i] = inputs[i];
+        if (inputs[i] > max)
+        {
+            max = inputs[i];
+        }
+    }
+    else
+    {
+        dp[i] = temp;
+    }
+    
+}
+
+Console.Write(max);
