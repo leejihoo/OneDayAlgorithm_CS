1d4e682de2d83b249547c149218b8375059bd6d5
leejihoo <ajtwlsdl33@gmail.com>
2023-08-04 20:56:59 +0900

    1965 상자넣기
    https://chestnut-vein-aab.notion.site/6f753bb3ce4944d78e0717f52c00db71?pvs=4

---
+++ OneDayAlgorithm_CS/1965.cs
@@ -0,0 +1,28 @@
+﻿int n = int.Parse(Console.ReadLine());
+int[] inputs = Array.ConvertAll(Console.ReadLine().Split(),int.Parse);
+int[] dp = new int[n+1];
+dp[1] = 1;
+for (int i = 2; i <= n; i++)
+{
+    int max = 1;
+    for (int j = 1; j < i; j++)
+    {
+        if (inputs[j-1] < inputs[i-1])
+        {
+            if (max < dp[j] + 1)
+            {
+                max = dp[j] + 1;
+            }
+        }
+        // else
+        // {
+        //     if (max < dp[j])
+        //     {
+        //         max = dp[j];
+        //     }
+        // }
+    }
+
+    dp[i] = max;
+}
+Console.Write(dp.Max());
