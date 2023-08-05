3f120e28ff8fb5efa215ce82262da6b8b867535a
leejihoo <ajtwlsdl33@gmail.com>
2023-08-05 23:55:21 +0900

    9184 신나는 함수 여행
    https://chestnut-vein-aab.notion.site/375f492d269142b480093013e1dc8096?pvs=4

---
+++ OneDayAlgorithm_CS/9184.cs
@@ -0,0 +1,79 @@
+﻿int[] inputs;
+long[,,] dp = new long[101, 101, 101];
+while (true)
+{
+    inputs = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+    if (inputs[0] == -1 && inputs[1] == -1 && inputs[2] == -1)
+    {
+        break;
+    }
+    Console.WriteLine($"w({inputs[0]}, {inputs[1]}, {inputs[2]}) = {w(inputs[0],inputs[1],inputs[2])}");
+}
+
+long w(int a, int b, int c)
+{
+    ref long save = ref dp[a + 50, b + 50, c + 50]; 
+    if (a <= 0 || b <= 0 || c <= 0)
+    {
+        save = 1;
+        return 1;
+    }
+
+    if (a > 20 || b > 20 || c > 20)
+    {
+        if (dp[70, 70, 70] == 0)
+        {
+            dp[70, 70, 70] = w(20, 20, 20);
+            save = dp[70,70,70];
+        }
+        else
+        {
+            save = dp[70, 70, 70];
+        }
+        
+        return save;
+    }
+
+    if (a < b && b < c)
+    {
+        if (dp[a + 50, b + 50, c + 49] == 0)
+        {
+            dp[a + 50, b + 50, c + 49] = w(a, b, c - 1);
+        }
+        
+        if (dp[a + 50, b + 49, c + 49] == 0)
+        {
+            dp[a + 50, b + 49, c + 49] = w(a, b - 1, c-1);
+        }
+        
+        if (dp[a + 50, b + 49, c + 50] == 0)
+        {
+            dp[a + 50, b + 49, c + 50] = w(a, b - 1, c);
+        }
+        
+        return dp[a + 50, b + 50, c + 49] + dp[a + 50, b + 49, c + 49] - dp[a + 50, b + 49, c + 50];
+        //return w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
+    }
+    
+    if (dp[a + 49, b + 50, c + 50] == 0)
+    {
+        dp[a + 49, b + 50, c + 50] = w(a-1, b, c);
+    }
+        
+    if (dp[a + 49, b + 49, c + 50] == 0)
+    {
+        dp[a + 49, b + 49, c + 50] = w(a-1, b - 1, c);
+    }
+        
+    if (dp[a + 49, b + 50, c + 49] == 0)
+    {
+        dp[a + 49, b + 50, c + 49] = w(a - 1, b, c - 1);
+    }
+    
+    if (dp[a + 49, b + 49, c + 49] == 0)
+    {
+        dp[a + 49, b + 49, c + 49] = w(a - 1, b-1, c - 1);
+    }
+
+    return dp[a + 49, b + 50, c + 50] + dp[a + 49, b + 49, c + 50] + dp[a + 49, b + 50, c + 49] - dp[a + 49, b + 49, c + 49];
+}
