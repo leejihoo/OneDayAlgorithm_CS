c6a82c29af9e01d84132adc0a2c0adff34481f1f
leejihoo <ajtwlsdl33@gmail.com>
2023-08-06 16:07:15 +0900

    11051 이항 계수2
    https://chestnut-vein-aab.notion.site/2-a6ecfe3b3752486c9c20c4b9219d495d?pvs=4

---
+++ OneDayAlgorithm_CS/11051.cs
@@ -0,0 +1,40 @@
+﻿int[] inputs = Array.ConvertAll(Console.ReadLine().Split(),int.Parse);
+long[,] dp = new long[1001,1001];
+
+
+long binomialCoefficient(int n, int k)
+{
+    if (dp[n, k] != 0)
+    {
+        return dp[n, k];
+    }
+
+    // if (n < k)
+    // {
+    //     return dp[n, k] = 0;
+    // }
+    
+    if (k == 0 || n == k)
+    {
+        return dp[n, k] = 1;
+    }
+    
+    if (k == 1)
+    {
+        return dp[n, k] = n;
+    }
+
+    return dp[n, k] = (binomialCoefficient(n-1,k-1) + binomialCoefficient(n-1,k)) % 10007;
+}
+
+long factorial(int n)
+{
+    if (n == 1)
+    {
+        return 1;
+    }
+    
+    return factorial(n - 1) * n;
+}
+
+Console.Write(binomialCoefficient(inputs[0],inputs[1]));
