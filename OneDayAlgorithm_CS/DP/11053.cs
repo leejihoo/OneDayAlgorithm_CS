e7076fb58af395630cad821a2417a9e7d2c9b8da
leejihoo <ajtwlsdl33@gmail.com>
2023-08-07 23:37:46 +0900

    11053 가장 긴 증가하는 부분 수열 ★
    https://chestnut-vein-aab.notion.site/173a89129ddb43b490e1df3b68d288c3?pvs=4

---
+++ OneDayAlgorithm_CS/DP/11053.cs
@@ -0,0 +1,27 @@
+﻿int N = int.Parse(Console.ReadLine());
+int[] inputs = Array.ConvertAll(Console.ReadLine().Split(),int.Parse);
+int[] dp = new int[N+1];
+Array.Fill(dp,1);
+
+for (int i = 2; i <= N; i++)
+{
+    for (int j = 1; j < i; j++)
+    {
+        if (inputs[j-1] < inputs[i-1])
+        {
+            if (dp[i] < dp[j] + 1)
+            {
+                dp[i] = dp[j] + 1;
+            }
+        }
+        // else
+        // {
+        //     if (dp[i] < dp[j])
+        //     {
+        //         dp[i] = dp[j];
+        //     }
+        // }
+    }
+}
+
+Console.Write(dp.Max());
