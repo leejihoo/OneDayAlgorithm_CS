dce6467c4f0618a1f975c09386bb18f02b76cee6
leejihoo <ajtwlsdl33@gmail.com>
2023-08-08 23:50:55 +0900

    11055 가장 큰 증가하는 부분 수열
    https://chestnut-vein-aab.notion.site/cef88e2499e04645a3052f713951d647?pvs=4

---
+++ OneDayAlgorithm_CS/DP/11055.cs
@@ -0,0 +1,30 @@
+﻿int N = int.Parse(Console.ReadLine());
+int[] inputs = Array.ConvertAll(Console.ReadLine().Split(),int.Parse);
+int[] dp = new int[N];
+dp[0] = inputs[0];
+for (int i = 1; i < N; i++)
+{
+    int max = 0;
+    for (int j = 0; j < i; j++)
+    {
+        if (inputs[j] < inputs[i])
+        {
+            if (max < dp[j] + inputs[i])
+            {
+                max = dp[j] + inputs[i];
+            }
+        }
+        
+    }
+
+    if (max != 0)
+    {
+        dp[i] = max;    
+    }
+    else
+    {
+        dp[i] = inputs[i];
+    }
+}
+
+Console.Write(dp.Max());
