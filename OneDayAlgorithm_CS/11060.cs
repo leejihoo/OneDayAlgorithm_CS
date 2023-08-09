509761098574c582675310a3325eb409071eab1a
leejihoo <ajtwlsdl33@gmail.com>
2023-08-09 23:34:08 +0900

    11060 점프 점프
    https://chestnut-vein-aab.notion.site/2d383031f740454792ff5c4b836da7a8?pvs=4

---
+++ OneDayAlgorithm_CS/11060.cs
@@ -0,0 +1,29 @@
+﻿int N = int.Parse(Console.ReadLine());
+int[] inputs = Array.ConvertAll(Console.ReadLine().Split(),int.Parse);
+int[]dp = new int[N];
+for (int i = 1; i < N; i++)
+{
+    int min = 1000;
+    for (int j = 0; j < i; j++)
+    {
+        
+        if (i - j <= inputs[j] && dp[j] >= 0)
+        {
+            if (min > dp[j] + 1)
+            {
+                min = dp[j] + 1;
+            }
+        }
+    }
+    
+    if (min == 1000)
+    {
+        dp[i] = -1;
+    }
+    else
+    {
+        dp[i] = min;
+    }
+}
+
+Console.Write(dp[N-1]);
