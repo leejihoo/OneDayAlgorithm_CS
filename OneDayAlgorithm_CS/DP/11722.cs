1082960d9e1588d2eda4de52c76c67096699cbc2
leejihoo <ajtwlsdl33@gmail.com>
2023-08-12 19:51:51 +0900

    11722 가장 긴 감소하는 부분 수열
    https://chestnut-vein-aab.notion.site/adfb059b2b694d86ab5ec1b36fc82815?pvs=4

---
+++ OneDayAlgorithm_CS/DP/11722.cs
@@ -0,0 +1,21 @@
+﻿int N = int.Parse(Console.ReadLine());
+int[] inputs = Array.ConvertAll(Console.ReadLine().Split(),int.Parse);
+int[] dp = new int[N];
+Array.Fill(dp,1);
+int min = 1001;
+
+for (int i = 1; i < N; i++)
+{
+    for (int j = 0; j < i; j++)
+    {
+        if (inputs[j] > inputs[i])
+        {
+            if (dp[i] < dp[j] + 1)
+            {
+                dp[i] = dp[j] + 1;
+            }
+        }
+    }
+}
+
+Console.WriteLine(dp.Max());
