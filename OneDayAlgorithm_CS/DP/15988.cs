60359f984dbdbf7fe24ade2f7441d79eedff5ced
leejihoo <ajtwlsdl33@gmail.com>
2023-08-13 23:32:31 +0900

    15988 1,2,3 더하기 3
    https://chestnut-vein-aab.notion.site/1-2-3-3-a0f59f8403f943a498e86adb6011888a?pvs=4

---
+++ OneDayAlgorithm_CS/DP/15988.cs
@@ -0,0 +1,15 @@
+﻿int T = int.Parse(Console.ReadLine());
+long[] dp = new long[1000001];
+dp[1] = 1;
+dp[2] = 2;
+dp[3] = 4;
+for (int i = 4; i < 1000001; i++)
+{
+    dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 3]) % 1000000009;
+}
+
+for (int i = 0; i < T; i++)
+{
+    int n = int.Parse(Console.ReadLine());
+    Console.WriteLine(dp[n]);
+}
