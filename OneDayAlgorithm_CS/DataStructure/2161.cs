cc6bb6832f701ad4a8c8249b13f131b5618dcc45
leejihoo <ajtwlsdl33@gmail.com>
2023-09-06 16:12:20 +0900

    [DataStructure] 2161 카드1
    https://chestnut-vein-aab.notion.site/1-83c2b3c882b348bda2d25e619dd7e942?pvs=4

---
+++ OneDayAlgorithm_CS/DataStructure/2161.cs
@@ -0,0 +1,27 @@
+﻿int N = int.Parse(Console.ReadLine());
+Queue<int> cards = new Queue<int>();
+StreamWriter streamWriter = new StreamWriter(Console.OpenStandardOutput());
+for (int i = 1; i <= N; i++)
+{
+    cards.Enqueue(i);
+}
+
+while (cards.Count > 0)
+{
+    int dead = cards.Dequeue();
+    
+    if (cards.Count > 0)
+    {
+        streamWriter.Write(dead+" ");
+        int move = cards.Dequeue();
+        cards.Enqueue(move);
+    }
+    else
+    {
+        streamWriter.Write(dead);
+    }
+    
+}
+
+streamWriter.Flush();
+streamWriter.Close();
