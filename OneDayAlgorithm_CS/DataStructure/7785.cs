446faefbe8afd66575c3890918b077dac47d8bc1
leejihoo <ajtwlsdl33@gmail.com>
2023-09-07 23:49:08 +0900

    [DataStructure] 7785 회사에 있는 사람
    https://chestnut-vein-aab.notion.site/33a6d87f6f1e453bbe57741f8e6c153a?pvs=4

---
+++ OneDayAlgorithm_CS/DataStructure/7785.cs
@@ -0,0 +1,32 @@
+﻿StreamReader streamReader = new StreamReader(Console.OpenStandardInput());
+StreamWriter streamWriter = new StreamWriter(Console.OpenStandardOutput());
+int N = int.Parse(streamReader.ReadLine());
+// Dictionary<string,string> log = new Dictionary<string,string>();
+// for (int i = 0; i < N; i++)
+// {
+//     string[] temp = streamReader.ReadLine().Split();
+//     log[temp[0]] = temp[1];
+// }
+//
+// var result = log.Where(x => x.Value == "enter").Select(x => x.Key).ToList();
+HashSet<string> ab = new HashSet<string>();
+
+for (int i = 0; i < N; i++)
+{
+    string[] temp = streamReader.ReadLine().Split();
+    if (!ab.Contains(temp[0]))
+    {
+        ab.Add(temp[0]);
+    }
+    else
+    {
+        ab.Remove(temp[0]);
+    }
+}
+
+foreach (var temp in ab.OrderByDescending(x=> x))
+{
+    streamWriter.WriteLine(temp);
+}
+
+streamWriter.Flush();
