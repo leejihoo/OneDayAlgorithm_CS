4ae2cde231d2e382d1bfa20def228cc1d16411fd
leejihoo <ajtwlsdl33@gmail.com>
2023-09-08 23:16:39 +0900

    [DataStructure] 3986 좋은 단어
    https://chestnut-vein-aab.notion.site/04628efc6ae741f3971693d780bcb996?pvs=4

---
+++ OneDayAlgorithm_CS/DataStructure/3986.cs
@@ -0,0 +1,35 @@
+﻿int N  = int.Parse(Console.ReadLine());
+int answer = 0;
+Stack<char> report = new Stack<char>();
+
+for (int i = 0; i < N; i++)
+{
+    
+    string temp = Console.ReadLine();
+    for (int j = 0; j < temp.Length; j++)
+    {
+        if (report.Count > 0)
+        {
+            if (report.Peek() == temp[j])
+            {
+                report.Pop();
+            }
+            else
+            {
+                report.Push(temp[j]);
+            }
+        }
+        else
+        {
+            report.Push(temp[j]);
+        }
+    }
+
+    if (report.Count == 0)
+    {
+        answer++;
+    }
+    report.Clear();
+}
+
+Console.Write(answer);
