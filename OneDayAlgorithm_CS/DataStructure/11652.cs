667445304f76d1304f99ab766c6cb91802b7f03e
leejihoo <ajtwlsdl33@gmail.com>
2023-09-09 23:48:30 +0900

    [DataStructure] 11652 카드
    https://chestnut-vein-aab.notion.site/dbde7fae703b4b57887b50366a04ed09?pvs=4

---
+++ OneDayAlgorithm_CS/DataStructure/11652.cs
@@ -0,0 +1,19 @@
+﻿int N = int.Parse(Console.ReadLine());
+Dictionary<long,int> cards = new Dictionary<long,int>();
+
+for (int i = 0; i < N; i++)
+{
+    long temp = long.Parse(Console.ReadLine());
+    if (cards.ContainsKey(temp))
+    {
+        cards[temp] += 1;
+    }
+    else
+    {
+        cards[temp] = 1;
+    }
+}
+
+var max = cards.Max(x => x.Value);
+var a = cards.Where(x => x.Value >= max).Min(y => y.Key);
+Console.Write(a);
