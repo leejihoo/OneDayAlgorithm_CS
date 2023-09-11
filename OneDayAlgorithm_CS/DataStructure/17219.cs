7cb04ee82fb880093023e0957f29f2aab54a6070
leejihoo <ajtwlsdl33@gmail.com>
2023-09-11 16:03:24 +0900

    [DataStructure] 17219 비밀번호 찾기
    https://chestnut-vein-aab.notion.site/4ab241b1f75343d8a14e88265f363315?pvs=4

---
+++ OneDayAlgorithm_CS/DataStructure/17219.cs
@@ -0,0 +1,23 @@
+﻿//StreamReader sr = new StreamReader(Console.OpenStandardInput());
+StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+Dictionary<string, string> program = new Dictionary<string, string>();
+
+//int[] inputs = Array.ConvertAll(sr.ReadLine().Split(),int.Parse);
+int[] inputs = Array.ConvertAll(Console.ReadLine().Split(),int.Parse);
+
+for (int i = 0; i < inputs[0]; i++)
+{
+    //var temp = sr.ReadLine().Split();
+    var temp = Console.ReadLine().Split();
+    program[temp[0]] = temp[1];
+}
+
+for (int i = 0; i < inputs[1]; i++)
+{
+    //var temp = sr.ReadLine();
+    var temp = Console.ReadLine();
+    sw.WriteLine(program[temp]);
+}
+
+sw.Close();
+//sr.Close();
