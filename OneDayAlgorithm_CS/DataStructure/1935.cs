97b61f13431975898a76212029f4360f4ae4d4f8
leejihoo <ajtwlsdl33@gmail.com>
2023-09-15 23:38:29 +0900

    [DataStructure] 1935 후위 표기식2
    https://chestnut-vein-aab.notion.site/2-356a51917d4a4872840d727dd129a310?pvs=4

---
+++ OneDayAlgorithm_CS/DataStructure/1935.cs
@@ -0,0 +1,40 @@
+﻿int N = int.Parse(Console.ReadLine());
+string postfix = Console.ReadLine();
+int[] operand = new int[N];
+Stack<decimal> stack = new Stack<decimal>();
+
+for (int i = 0; i < N; i++)
+{
+    operand[i] = int.Parse(Console.ReadLine());
+}
+
+for (int i = 0; i < postfix.Length; i++)
+{
+    var temp = postfix[i] - 'A';
+    if ( temp >= 0 && temp <= 25)
+    {
+        stack.Push(operand[temp]);
+        continue;
+    }
+
+    decimal back = stack.Pop();
+    decimal front = stack.Pop();
+    if (postfix[i] == '*')
+    {
+        stack.Push(front*back);
+    }
+    else if (postfix[i] == '+')
+    {
+        stack.Push(front+back);
+    }
+    else if (postfix[i] == '/')
+    {
+        stack.Push(front/back);
+    }
+    else if (postfix[i] == '-')
+    {
+        stack.Push(front-back);
+    }
+}
+
+Console.Write(Decimal.Round(stack.Pop(),2).ToString("0.00"));
