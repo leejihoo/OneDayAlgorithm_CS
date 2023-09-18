e0f6cfa7ac30c0199a601035f6b4bf2915fbb3f5
leejihoo <ajtwlsdl33@gmail.com>
2023-09-18 19:29:54 +0900

    [DataStructure] 1966 프린터 큐
    https://chestnut-vein-aab.notion.site/a6777694fc7f458c81acf2277c658cdb?pvs=4

---
+++ OneDayAlgorithm_CS/DataStructure/1966.cs
@@ -0,0 +1,45 @@
+﻿StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+int T = int.Parse(Console.ReadLine());
+Queue<int> priority = new Queue<int>();
+Queue<int> index = new Queue<int>();
+
+for (int i = 0; i < T; i++)
+{
+    string[] inputs = Console.ReadLine().Split();
+    int answer = int.Parse(inputs[1]);
+    int[] docs = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+    int count = docs.Length;
+    for (int j = 0; j < count; j++)
+    {
+        priority.Enqueue(docs[j]);
+        index.Enqueue(j);
+    }
+    
+    for (int j = 0; j < count; j++)
+    {
+        int max = priority.Max();
+
+        int idx = index.Dequeue();
+        int pri = priority.Dequeue();
+        
+        if (pri == max)
+        {
+            if (answer == idx)
+            {
+                sw.WriteLine(j+1);
+                break;
+            }
+
+            continue;
+        }
+
+        j--;
+        index.Enqueue(idx);
+        priority.Enqueue(pri);
+    }
+    
+    priority.Clear();
+    index.Clear();
+}
+
+sw.Close();
