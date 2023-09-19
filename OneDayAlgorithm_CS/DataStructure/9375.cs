217ca35537de6438986e5c8a26c72dd2de269ae2
leejihoo <ajtwlsdl33@gmail.com>
2023-09-19 14:23:57 +0900

    [DataStructure] 9375 패션왕 신해빈
    https://chestnut-vein-aab.notion.site/ccb3230501034d94aead4c463ee90d1a?pvs=4

---
+++ OneDayAlgorithm_CS/DataStructure/9375.cs
@@ -0,0 +1,39 @@
+﻿StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+
+int T = int.Parse(Console.ReadLine());
+
+Dictionary<string, int> fashion = new Dictionary<string, int>();
+
+while (T > 0)
+{
+    T--;
+    int n = int.Parse(Console.ReadLine());
+    
+    while (n > 0)
+    {
+        n--;
+        string[] temp = Console.ReadLine().Split();
+        
+        if (fashion.ContainsKey(temp[1]))
+        {
+            fashion[temp[1]] += 1;
+            continue;
+        }
+        
+        fashion.Add(temp[1],1);    
+        
+    }
+
+    if (fashion.Count > 0)
+    {
+        var answer = fashion.Values.Select(x=> x+1).Aggregate((x,y)=>x*y) -1;
+        sw.WriteLine(answer);    
+        fashion.Clear();
+        continue;
+    }
+    
+    sw.WriteLine(0);
+
+}
+
+sw.Close();
