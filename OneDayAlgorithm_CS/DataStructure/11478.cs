3729cca1bd92425ddc1ebebf09769b43e9064c8b
leejihoo <ajtwlsdl33@gmail.com>
2023-09-19 14:57:28 +0900

    [DataStructure] 11478 서로 다른 부분 문자열의 개수
    https://chestnut-vein-aab.notion.site/427fd17a1eb64725b0f49cdae864a09b?pvs=4

---
+++ OneDayAlgorithm_CS/DataStructure/11478.cs
@@ -0,0 +1,11 @@
+﻿string str = Console.ReadLine();
+HashSet<string> diffStr = new HashSet<string>();
+int length = str.Length;
+for (int i = 0; i < length; i++)
+{
+    for (int j = 0; j < length-i; j++)
+    {
+        diffStr.Add(str.Substring(i, j+1));
+    }
+}
+Console.Write(diffStr.Count);
