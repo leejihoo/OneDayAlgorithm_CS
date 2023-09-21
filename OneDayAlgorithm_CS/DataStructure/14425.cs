fce2b58ba7b4037eea8a80294c83a84e66a4a940
leejihoo <ajtwlsdl33@gmail.com>
2023-09-21 23:10:31 +0900

    [DataStructure] 14425 문자열 집합
    https://chestnut-vein-aab.notion.site/228955414c3349d99c8df6c456a89e6b?pvs=4

---
+++ OneDayAlgorithm_CS/DataStructure/14425.cs
@@ -0,0 +1,25 @@
+﻿int[] inputs = Array.ConvertAll(Console.ReadLine().Split(),int.Parse);
+HashSet<string> setS = new HashSet<string>();
+List<string> inspectingStr = new List<string>();
+int count = 0;
+
+for (int i = 0; i < inputs[0]; i++)
+{
+    setS.Add(Console.ReadLine());
+}
+for (int i = 0; i < inputs[1]; i++)
+{
+    inspectingStr.Add(Console.ReadLine());
+}
+
+foreach (var str in inspectingStr)
+{
+    if (setS.Contains(str))
+    {
+        count++;
+    }
+}
+
+Console.Write(count);
+
+
