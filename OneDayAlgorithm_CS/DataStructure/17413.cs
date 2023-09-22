862841250071fcdd49842f9c87835dd26498be58
leejihoo <ajtwlsdl33@gmail.com>
2023-09-22 22:14:34 +0900

    [DataStructure] 17413 단어 뒤집기2
    https://chestnut-vein-aab.notion.site/2-311e15593825402b9b3bd0fc31d07e9b?pvs=4

---
+++ OneDayAlgorithm_CS/DataStructure/17413.cs
@@ -0,0 +1,62 @@
+﻿using System.Text;
+
+StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+StringBuilder sb = new StringBuilder();
+string S = Console.ReadLine();
+Queue<char> queue = new Queue<char>();
+Stack<char> stack = new Stack<char>();
+bool isTag = false;
+for (int i = 0; i < S.Length; i++)
+{
+    if ((S[i] >= 'a' && S[i] <= 'z') || (S[i] >= '0' && S[i] <= '9'))
+    {
+        if (isTag)
+        {
+            sb.Append(S[i]);
+            continue;
+        }
+        
+        stack.Push(S[i]);
+        if (i == S.Length - 1)
+        {
+            while (stack.Count > 0)
+            {
+                sb.Append(stack.Pop());
+            }
+        }
+    }
+    else if (S[i] == '<')
+    {
+        isTag = true;
+
+        while (stack.Count > 0)
+        {
+            sb.Append(stack.Pop());
+        }
+
+        sb.Append('<');
+    }
+    else if (S[i] == '>')
+    {
+        isTag = false;
+        sb.Append('>');
+    }
+    else if (S[i] == ' ')
+    {
+        if (isTag)
+        {
+            sb.Append(S[i]);
+            continue;
+        }
+        
+        while (stack.Count > 0)
+        {
+            sb.Append(stack.Pop());
+        }
+
+        sb.Append(' ');
+    }
+}
+
+sw.Write(sb.ToString());
+sw.Close();
