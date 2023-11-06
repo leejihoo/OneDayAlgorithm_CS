ed4a09f0827a46cd87536b293fca3cfce4ee9b23
leejihoo <ajtwlsdl33@gmail.com>
2023-11-06 13:21:52 +0900

    [DataStructure] 1927 최소 힙
    https://chestnut-vein-aab.notion.site/590bfe428f244139a4339e6f4a408a1f?pvs=4

---
+++ OneDayAlgorithm_CS/DataStructure/1927.cs
@@ -0,0 +1,27 @@
+﻿PriorityQueue<int, int> priorityQueue = new PriorityQueue<int, int>();
+StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+int count = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < count; i++)
+{
+    int temp = int.Parse(Console.ReadLine());
+
+    if (temp == 0)
+    {
+        if (priorityQueue.Count == 0)
+        {
+            sw.WriteLine(0);
+            continue;
+        }
+
+        sw.WriteLine(priorityQueue.Dequeue());
+    }
+    else
+    {
+        priorityQueue.Enqueue(temp,temp);
+    }
+    
+}
+
+sw.Flush();
+sw.Close();
