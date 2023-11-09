dfaf54e7074805915232658748a4435ce8d9c9af
leejihoo <ajtwlsdl33@gmail.com>
2023-11-09 13:44:34 +0900

    [DataStructure]2075 N번째 큰 수
    https://chestnut-vein-aab.notion.site/N-4413cda50055413eb0e98d662e23f4a1?pvs=4

---
+++ OneDayAlgorithm_CS/DataStructure/2075.cs
@@ -0,0 +1,15 @@
+﻿StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+int N = int.Parse(Console.ReadLine());
+List<int> listN = new List<int>(); 
+for (int i = 0; i < N; i++)
+{
+    var temp = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+    listN.AddRange(temp);
+}
+
+listN.Sort();
+listN.Reverse();
+
+sw.WriteLine(listN[N-1]);
+sw.Flush();
+sw.Close();
