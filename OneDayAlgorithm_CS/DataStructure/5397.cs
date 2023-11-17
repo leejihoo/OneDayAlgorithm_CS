aac7dddeac3c3ace520a13835da222103608f2a9
leejihoo <ajtwlsdl33@gmail.com>
2023-11-17 13:26:57 +0900

    [DataStructure] 5397 키로거
    https://chestnut-vein-aab.notion.site/937f360c5e714cbc87f7cd6453b9cb6a?pvs=4

---
+++ OneDayAlgorithm_CS/DataStructure/5397.cs
@@ -0,0 +1,63 @@
+﻿using System.Text;
+
+StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+StringBuilder sb = new StringBuilder();
+
+Stack<char> front = new Stack<char>();
+Stack<char> back = new Stack<char>();
+
+int t = int.Parse(sr.ReadLine());
+
+for (int i = 0; i < t; i++)
+{
+    string input = sr.ReadLine();
+    foreach (var s in input)
+    {
+        if (s == '<')
+        {
+            if (front.Count > 0)
+            {
+                back.Push(front.Pop());
+            }
+        }
+        else if (s == '>')
+        {
+            if (back.Count > 0)
+            {
+                front.Push(back.Pop());
+            } 
+        }
+        else if (s == '-')
+        {
+            if (front.Count > 0)
+            {
+                front.Pop();
+            }
+        }
+        else
+        {
+            front.Push(s);
+        }
+    }
+
+    var reverseF = front.Reverse();
+    foreach (var f in reverseF)
+    {
+        sb.Append(f);
+    }
+
+    foreach (var b in back)
+    {
+        sb.Append(b);
+    }
+    sw.WriteLine(sb.ToString());
+    
+    sb.Clear();
+    front.Clear();
+    back.Clear();
+}
+
+sw.Flush();
+sw.Close();
+sr.Close();
