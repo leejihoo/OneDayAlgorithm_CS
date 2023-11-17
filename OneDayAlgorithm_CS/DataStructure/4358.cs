bead54389b3a3902bab0c76d4d844218aad6e15f
leejihoo <ajtwlsdl33@gmail.com>
2023-11-17 12:52:16 +0900

    [DataStructure] 4358 생태학
    https://chestnut-vein-aab.notion.site/d80f5ddfb89b42f1800471d6840e6cad?pvs=4

---
+++ OneDayAlgorithm_CS/DataStructure/4358.cs
@@ -0,0 +1,32 @@
+﻿StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+SortedDictionary<string,int> tree = new SortedDictionary<string, int>();
+int treeCount = 0;
+
+while (true)
+{
+    string treeName;
+
+    treeName = sr.ReadLine();
+    
+    if(treeName == null) break;
+    //if (treeName == null && sr.Peek() == -1) break;
+    treeCount++;
+    if (tree.ContainsKey(treeName))
+    {
+        tree[treeName]++;
+        continue;
+    }
+
+    tree[treeName] = 1;
+
+}
+
+foreach (var t in tree)
+{
+    sw.WriteLine(t.Key + " " + ((t.Value / (float)treeCount) * 100f).ToString("F4"));
+}
+
+sw.Flush();
+sr.Close();
+sw.Close();
