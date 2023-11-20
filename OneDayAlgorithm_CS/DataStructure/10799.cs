49fe22d5c9ad1e338d8b5db53712ebfce5801d5d
leejihoo <ajtwlsdl33@gmail.com>
2023-11-20 17:03:05 +0900

    [DataStructure] 10799 쇠막대기
    https://chestnut-vein-aab.notion.site/e09b838614f9416fb486540dfd19011e?pvs=4

---
+++ OneDayAlgorithm_CS/DataStructure/10799.cs
@@ -0,0 +1,36 @@
+﻿StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+Stack<char> stick = new Stack<char>();
+
+string input = sr.ReadLine();
+char prev = '(';
+int answer = 0;
+
+foreach (var s in input)
+{
+    if (s == '(')
+    {
+        stick.Push(s);
+        prev = s;
+        continue;
+    }
+
+    stick.Pop();
+    
+    if (prev == '(')
+    {
+        answer += stick.Count;
+    }
+    else
+    {
+        answer += 1;
+    }
+    
+    prev = s;
+}
+
+sw.Write(answer);
+sw.Flush();
+sw.Close();
+sr.Close();
