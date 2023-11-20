c067e3d3fd015165c4df65773ae4bd36e3d2e944
leejihoo <ajtwlsdl33@gmail.com>
2023-11-20 17:22:17 +0900

    [DataStructure] 11279 최대힙
    https://chestnut-vein-aab.notion.site/044acd53242248e69dccef331e51aee9?pvs=4

---
+++ OneDayAlgorithm_CS/DataStructure/11279.cs
@@ -0,0 +1,29 @@
+﻿StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+
+PriorityQueue<int,double> maxHeap = new PriorityQueue<int, double>();
+
+int count = int.Parse(sr.ReadLine());
+
+for (int i = 0; i < count; i++)
+{
+    int input = int.Parse(sr.ReadLine());
+    
+    if (input == 0)
+    {
+        if (maxHeap.Count == 0)
+        {
+            sw.WriteLine(0);
+            continue;
+        }
+        
+        sw.WriteLine(maxHeap.Dequeue());
+        continue;
+    }
+    
+    maxHeap.Enqueue(input, 1 / (double)input);
+}
+
+sw.Flush();
+sw.Close();
+sr.Close();
