77ef7e5078e7084ef47f9f2fb3a0dccf77b946b9
leejihoo <ajtwlsdl33@gmail.com>
2023-11-23 15:17:40 +0900

    [DataStructure] 11286 절대값 힙
    https://chestnut-vein-aab.notion.site/39f860771e884b688f7d092097f988f6?pvs=4

---
+++ OneDayAlgorithm_CS/DataStructure/11286.cs
@@ -0,0 +1,37 @@
+﻿PriorityQueue<int, int> priorityQueue = new PriorityQueue<int, int>(Comparer<int>.Create( (x,y) =>
+{
+    if (Math.Abs(x).CompareTo(Math.Abs(y)) != 0)
+    {
+        return Math.Abs(x).CompareTo(Math.Abs(y));
+    }
+
+    return x.CompareTo(y);
+
+}));
+
+StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+int count = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < count; i++)
+{
+    int temp = int.Parse(Console.ReadLine());
+
+    if (temp == 0)
+    {
+        if (priorityQueue.Count == 0)
+        {
+            sw.WriteLine(0);
+            continue;
+        }
+
+        sw.WriteLine(priorityQueue.Dequeue());
+    }
+    else
+    {
+        priorityQueue.Enqueue(temp,temp);
+    }
+    
+}
+
+sw.Flush();
+sw.Close();
