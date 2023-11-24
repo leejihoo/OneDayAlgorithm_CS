fc858f3a5c6acb0f03354099e8552f68b65a3d6f
leejihoo <ajtwlsdl33@gmail.com>
2023-11-24 23:51:35 +0900

    [DataStructure] 13335 트럭 ★
    https://chestnut-vein-aab.notion.site/6186f3541b9a4fbb8748d29a1587aad2?pvs=4

---
+++ OneDayAlgorithm_CS/DataStructure/13335.cs
@@ -0,0 +1,43 @@
+﻿using System.Globalization;
+
+StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+
+Queue<int> after = new Queue<int>();
+Queue<int> before = new Queue<int>();
+
+int sum = 0;
+int answer = 0;
+int[] input = Array.ConvertAll(sr.ReadLine().Split(),int.Parse);
+var trucks = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
+
+foreach (var t in trucks)
+{
+    before.Enqueue(t);
+}
+
+while (before.Count > 0)
+{
+    answer += 1;
+    if (after.Count == input[1])
+    {
+        var truck = after.Dequeue();
+        sum -= truck;
+    }
+
+    if (sum + before.Peek() > input[2])
+    {
+        after.Enqueue(0);
+        continue;
+    }
+    var temp = before.Dequeue();
+    after.Enqueue(temp);
+    sum += temp;
+    
+}
+
+// 길이가 w인 다리를 트럭이 건널려면 w만큼의 시간이 걸린다. 
+sw.Write(answer + input[1]);
+sw.Flush();
+sw.Close();
+sr.Close();
