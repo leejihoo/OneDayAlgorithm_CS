66da1cae66a236a5b7a0a6bccedb025dc2f47833
leejihoo <ajtwlsdl33@gmail.com>
2023-11-25 22:32:12 +0900

    [DataStructure] 15903 카드 합체 놀이
    https://chestnut-vein-aab.notion.site/62f374b2c36d41b6adbacab6d159f35f?pvs=4

---
+++ OneDayAlgorithm_CS/DataStructure/15903.cs
@@ -0,0 +1,18 @@
+﻿StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+
+long[] mn = Array.ConvertAll(sr.ReadLine().Split(),long.Parse);
+List<long> cards = Array.ConvertAll(sr.ReadLine().Split(),long.Parse).ToList();
+
+for (int i = 0; i < mn[1]; i++)
+{
+    cards.Sort();
+    var sum = cards[0] + cards[1];
+    cards[0] = sum;
+    cards[1] = sum;
+}
+
+sw.Write(cards.Sum());
+sw.Flush();
+sw.Close();
+sr.Close();
