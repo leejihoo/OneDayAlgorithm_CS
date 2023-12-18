33cf8eac4f523b4e8e178acc6ca2a3bb3834f47f
leejihoo <ajtwlsdl33@gmail.com>
2023-12-18 18:06:35 +0900

    [DisjointSet] 20040 사이클 게임
    https://chestnut-vein-aab.notion.site/baffc5339a414d7ebd777580b098562a?pvs=4

---
+++ OneDayAlgorithm_CS/DataStructure/DisJointSet/20040.cs
@@ -0,0 +1,61 @@
+﻿StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+StreamReader sr = new StreamReader(Console.OpenStandardInput());
+
+int[] nm = Array.ConvertAll(sr.ReadLine().Split(),int.Parse);
+int[] parent = new int[nm[0]];
+int answer = 0;
+
+for (int i = 1; i < parent.Length; i++)
+{
+    parent[i] = i;
+}
+
+for (int i = 0; i < nm[1]; i++)
+{
+    int[] edge = Array.ConvertAll(sr.ReadLine().Split(),int.Parse);
+    var isCircle = Union(edge[0], edge[1]);
+
+    if (isCircle)
+    {
+        answer = i + 1;
+        break;
+    }
+}
+
+sw.Write(answer);
+sw.Flush();
+sw.Close();
+sr.Close();
+
+bool Union(int first, int second)
+{
+    int firstP = GetParent(first);
+    int secondP = GetParent(second);
+
+    if (firstP == secondP)
+    {
+        return true;
+    }
+
+    if (firstP > secondP)
+    {
+        parent[firstP] = secondP;
+    }
+    else
+    {
+        parent[secondP] = firstP;
+    }
+    
+    return false;
+}
+
+int GetParent(int num)
+{
+    if (parent[num] == num)
+    {
+        return num;
+    }
+
+    return parent[num] = GetParent(parent[num]);
+}
+
