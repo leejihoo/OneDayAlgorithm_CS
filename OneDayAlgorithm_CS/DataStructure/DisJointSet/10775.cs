530fe04ed087d2995988692e5c1b6c8696118521
leejihoo <ajtwlsdl33@gmail.com>
2023-12-23 22:05:09 +0900

    [DIsJointSet] 10775 공항
    https://chestnut-vein-aab.notion.site/47bb995978f0463491c73b034efa87de?pvs=4

---
+++ OneDayAlgorithm_CS/DataStructure/DisJointSet/10775.cs
@@ -0,0 +1,67 @@
+﻿StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+StreamReader sr = new StreamReader(Console.OpenStandardInput());
+
+int G = int.Parse(sr.ReadLine());
+int P = int.Parse(sr.ReadLine());
+int[] parent = new int[100001];
+int answer = 0;
+bool isClose = false;
+for (int i = 1; i < parent.Length; i++)
+{
+    parent[i] = i;
+}
+
+for (int i = 0; i < P; i++)
+{
+    int gi = int.Parse(sr.ReadLine());
+    if (isClose)
+    {
+        continue;
+    }
+    
+    if (GetParent(gi) != 0)
+    {
+        answer++;
+        var p = GetParent(gi);
+        Union(p,p-1);
+        continue;
+    }
+
+    isClose = true;
+}
+
+sw.Write(answer);
+sw.Flush();
+sw.Close();
+sr.Close();
+
+int GetParent(int num)
+{
+    if (parent[num] == num)
+    {
+        return num;
+    }
+
+    return parent[num] = GetParent(parent[num]);
+}
+
+void Union(int first, int second)
+{
+    int firstP = GetParent(first);
+    int secondP = GetParent(second);
+
+    if (firstP == secondP)
+    {
+        return;
+    }
+
+    if (firstP > secondP)
+    {
+        parent[firstP] = secondP;
+        return;
+    }
+
+    parent[secondP] = firstP;
+    
+}
+
