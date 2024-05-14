360497c5481a1005655160c221d6e01975aafa0a
leejihoo <ajtwlsdl33@gmail.com>
2024-05-14 16:20:58 +0900

    [Dijkstra] 13549 숨바꼭질 ★
    https://chestnut-vein-aab.notion.site/3-618bd202d0174e0083549ab40c81da41?pvs=4

---
+++ OneDayAlgorithm_CS/ShortestPath/Dijkstra/13549.cs
@@ -0,0 +1,56 @@
+﻿StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+StreamReader sr = new StreamReader(Console.OpenStandardInput());
+
+int answer = 100001;
+PriorityQueue<int,int> priorityQueue = new PriorityQueue<int, int>();
+bool[] check = new bool[100001];
+
+Array.Fill(check,false);
+
+var nm = Array.ConvertAll(sr.ReadLine().Split(),int.Parse);
+
+priorityQueue.Enqueue(nm[0],0);
+
+while (priorityQueue.Count > 0)
+{
+    priorityQueue.TryDequeue(out int next, out int priority);
+
+    if (next > 100000 || next < 0)
+    {
+        continue;
+    }
+    
+    if (check[next])
+    {
+        continue;
+    }
+
+    check[next] = true;
+    
+    if (priority >= answer)
+    {
+        break;
+    }
+    
+    if (nm[1] == next)
+    {
+        answer = priority;
+    }
+    else if (nm[1] > next)
+    {
+        priorityQueue.Enqueue(next+1,priority+1);
+        priorityQueue.Enqueue(next-1,priority+1);
+        priorityQueue.Enqueue(next*2,priority);
+    }
+    else
+    {
+        if (next - nm[1] + priority < answer)
+        {
+            answer = next - nm[1] + priority;
+        }
+    }
+}
+
+sw.Write(answer);
+sw.Flush();
+sw.Close();
