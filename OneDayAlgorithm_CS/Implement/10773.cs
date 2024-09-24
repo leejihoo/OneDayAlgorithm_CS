63c78e9ee8c50ec1fe9a9906c74e72b13ddcb54b
leejihoo <ajtwlsdl33@gmail.com>
2024-09-24 22:25:25 +0900

    [Implement] 10773 제로
    https://chestnut-vein-aab.notion.site/10ba29e9c2dc80bcacf8db16ea51fd24?pvs=4

---
+++ OneDayAlgorithm_CS/Implement/10773.cs
@@ -0,0 +1,27 @@
+﻿int k = int.Parse(Console.ReadLine());
+Stack<int> numbers = new Stack<int>();
+
+while (k > 0)
+{
+    k--;
+    
+    int input = int.Parse(Console.ReadLine());
+    if (input == 0)
+    {
+        numbers.Pop();
+        continue;
+    }
+    
+    numbers.Push(input);
+}
+
+int sum = 0;
+int count = numbers.Count;
+
+for (int i = 0; i < count; i++)
+{
+    int element = numbers.Pop();
+    sum += element;
+}
+
+Console.WriteLine(sum);
