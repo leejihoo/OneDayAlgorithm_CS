0b1c36f778ca5b0b5c18d7725abe63f98c38af84
leejihoo <ajtwlsdl33@gmail.com>
2024-09-25 23:21:59 +0900

    [Implement] 7568 덩치
    https://chestnut-vein-aab.notion.site/10ca29e9c2dc80b2aee9f32836c85c16?pvs=4

---
+++ OneDayAlgorithm_CS/Implement/7568.cs
@@ -0,0 +1,38 @@
+﻿int N = int.Parse(Console.ReadLine());
+BodyInfo[] bodyInfos = new BodyInfo[N];
+int[] rank = new int[N];
+Array.Fill(rank,1);
+
+for (int i = 0; i < N; i++)
+{
+    var input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+    bodyInfos[i] = new BodyInfo();
+    bodyInfos[i].weight = input[0];
+    bodyInfos[i].height = input[1];
+}
+
+for (int i = 0; i < N; i++)
+{
+    for (int j = i+1; j < N; j++)
+    {
+        if (bodyInfos[i].weight > bodyInfos[j].weight && bodyInfos[i].height > bodyInfos[j].height)
+        {
+            rank[j] += 1;
+            continue;
+        }
+        
+        if (bodyInfos[i].weight < bodyInfos[j].weight && bodyInfos[i].height < bodyInfos[j].height)
+        {
+            rank[i] += 1;
+        }
+    }
+}
+
+Console.WriteLine(String.Join(" ", rank));
+
+
+public class BodyInfo
+{
+    public int weight;
+    public int height;
+}
