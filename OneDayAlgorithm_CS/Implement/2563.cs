dcb890208059db2e89b3399a1b699b71bb4a0d3a
leejihoo <ajtwlsdl33@gmail.com>
2024-09-27 00:36:13 +0900

    [Implement] 2563 색종이★
    https://chestnut-vein-aab.notion.site/10da29e9c2dc80939f0ef1d23156ffb2?pvs=4

---
+++ OneDayAlgorithm_CS/Implement/2563.cs
@@ -0,0 +1,38 @@
+﻿int count = int.Parse(Console.ReadLine());
+Size[] colorPaperSizes = new Size[count];
+bool[,] check = new bool[100, 100];
+for (int i = 0; i < colorPaperSizes.Length; i++)
+{
+    var input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+    colorPaperSizes[i] = new Size();
+    colorPaperSizes[i].x = input[0];
+    colorPaperSizes[i].y = input[1];
+}
+
+for (int i = 0; i < colorPaperSizes.Length; i++)
+{
+    for (int x = colorPaperSizes[i].x; x < colorPaperSizes[i].x + 10; x++)
+    {
+        for (int y = colorPaperSizes[i].y; y < colorPaperSizes[i].y+10; y++)
+        {
+            check[x, y] = true;
+        }
+    }
+}
+
+int sum = 0;
+foreach (var isOverlaped in check)
+{
+    if (isOverlaped)
+    {
+        sum += 1;
+    }
+}
+
+Console.WriteLine(sum);
+
+public class Size
+{
+    public int x;
+    public int y;
+}
