fd599e7c674706c11c6c845a300438f5cd3f48b9
leejihoo <ajtwlsdl33@gmail.com>
2024-09-27 23:07:02 +0900

    [Implement] 2108 통계학
    https://chestnut-vein-aab.notion.site/10ea29e9c2dc809794ebe20809dab698?pvs=4

---
+++ OneDayAlgorithm_CS/Implement/2108.cs
@@ -0,0 +1,67 @@
+﻿int N = int.Parse(Console.ReadLine());
+
+List<int> numbers = new List<int>();
+Dictionary<int, int> count = new Dictionary<int, int>();
+for (int i = 0; i < N; i++)
+{
+    var input = int.Parse(Console.ReadLine());
+    numbers.Add(input);
+}
+
+numbers.Sort();
+
+for (int i = 0; i < N; i++)
+{
+    if (count.ContainsKey(numbers[i]))
+    {
+        count[numbers[i]] += 1;
+        continue;
+    }
+    
+    count[numbers[i]] = 1;
+}
+
+int max = 0;
+List<int> modes = new List<int>();
+var set = numbers.ToHashSet();
+
+foreach (var num in set)
+{
+    if (max < count[num])
+    {
+        max = count[num];
+        modes.Clear();
+        modes.Add(num);
+        continue;
+    }
+
+    if (max == count[num])
+    {
+        modes.Add(num);
+    }
+}
+modes.Sort();
+
+var output = Math.Round((double)numbers.Sum() / N);
+if (output == 0)
+{
+    Console.WriteLine(Math.Abs(output));
+}
+else
+{
+    Console.WriteLine(output);
+}
+
+
+Console.WriteLine(numbers[N/2]);
+
+if (modes.Count > 1)
+{
+    Console.WriteLine(modes[1]);
+}
+else
+{
+    Console.WriteLine(modes[0]);
+}
+
+Console.WriteLine(numbers[N-1] - numbers[0]);
