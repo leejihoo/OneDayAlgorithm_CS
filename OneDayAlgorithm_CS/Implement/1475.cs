7cbdf58859ed5e96d4f2fba3e97edd5036ead2f4
leejihoo <ajtwlsdl33@gmail.com>
2024-09-28 23:20:20 +0900

    [Implementation] 1475 방 번호
    https://chestnut-vein-aab.notion.site/10fa29e9c2dc8039bd87efe0d7a377bc?pvs=4

---
+++ OneDayAlgorithm_CS/Implement/1475.cs
@@ -0,0 +1,37 @@
+﻿string input = Console.ReadLine();
+int[] counts = new int[9];
+
+foreach (var num in input)
+{
+    if (num == '6' || num == '9')
+    {
+        counts[6] += 1;
+        continue;
+    }
+
+    counts[num - '0'] += 1;
+}
+
+int maxCount = 0;
+int index = 0;
+foreach (var count in counts)
+{
+    if (count > maxCount)
+    {
+        if (index == 6)
+        {
+            if ((count + 1) / 2 > maxCount)
+            {
+                maxCount = (count + 1) / 2;
+            }
+        }
+        else
+        {
+            maxCount = count;
+        }
+    }
+
+    index++;
+}
+
+Console.WriteLine(maxCount);
