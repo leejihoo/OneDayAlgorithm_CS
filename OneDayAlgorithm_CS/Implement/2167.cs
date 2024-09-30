50cc8050191fb4a6b864d9a28f4a4fa4d755d674
leejihoo <ajtwlsdl33@gmail.com>
2024-09-30 21:45:17 +0900

    [Implementation] 2167 2차원 배열의 합
    https://chestnut-vein-aab.notion.site/2-111a29e9c2dc80e9a64ac7433778f9d3?pvs=4

---
+++ OneDayAlgorithm_CS/Implement/2167.cs
@@ -0,0 +1,29 @@
+﻿var NM = Array.ConvertAll(Console.ReadLine().Split(),int.Parse);
+
+int[,] arr = new int[NM[0],NM[1]];
+
+for (int i = 0; i < NM[0]; i++)
+{
+    var input = Array.ConvertAll(Console.ReadLine().Split(),int.Parse);
+    for (int j = 0; j < NM[1]; j++)
+    {
+        arr[i, j] = input[j];
+    }
+}
+
+var K = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < K; i++)
+{
+    int sum = 0;
+    var input = Array.ConvertAll(Console.ReadLine().Split(),int.Parse);
+    for (int j = input[0]-1; j < input[2]; j++)
+    {
+        for (int k = input[1]-1; k < input[3]; k++)
+        {
+            sum += arr[j, k];
+        }
+    }
+    
+    Console.WriteLine(sum);
+}
