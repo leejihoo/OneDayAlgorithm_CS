7a840b7b1e618fa715d0cf985f1d4015aa78caa8
leejihoo <ajtwlsdl33@gmail.com>
2024-10-01 21:26:39 +0900

    [Implementation] 2960 에라토스테네스의 체
    https://chestnut-vein-aab.notion.site/112a29e9c2dc80a1ae16d68518fa19aa?pvs=4

---
+++ OneDayAlgorithm_CS/Implement/2960.cs
@@ -0,0 +1,31 @@
+﻿var input = Array.ConvertAll(Console.ReadLine().Split(),int.Parse);
+
+bool[] check = new bool[input[0]+1];
+int answer = 0;
+int count = 0;
+bool isFind = false;
+for (int i = 2; i < input[0]+1; i++)
+{
+    for (int j = 1; i*j < input[0]+1; j++)
+    {
+        if (check[i * j] == false)
+        {
+            count++;
+            check[i * j] = true;
+
+            if (count == input[1])
+            {
+                answer = i * j;
+                isFind = true;
+                break;
+            }
+        }
+    }
+
+    if (isFind)
+    {
+        break;
+    }
+}
+
+Console.WriteLine(answer);
